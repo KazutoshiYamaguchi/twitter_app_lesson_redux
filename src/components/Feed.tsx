d6118c32cdc86d9f4ac55f737c03c21d015405b2
kazutoshi <umaibou300pon@icloud.com>
2022-05-03 13:35:48 +0900

    git branch -M main

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,10 +1,58 @@
-import React from "react";
-import { auth } from "../firebase";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { TweetInput } from "./TweetInput";
+import styles from "./Feed.module.css";
+import { Post } from "./Post";
 
-export const Feed = () => {
+export const Feed: React.FC = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: "",
+      avator: "",
+      image: "",
+      text: "",
+      timestamp: null,
+      username: "",
+    },
+  ]);
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avator: doc.data().avatar,
+            image: doc.data().image,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().username,
+          }))
+        )
+      );
+    return () => {
+      unSub();
+    };
+  }, []);
   return (
-    <div>
-      <button onClick={() => auth.signOut()}>Logout</button>
+    <div className={styles.feed}>
+      <TweetInput />
+      {posts[0]?.id && (
+        <>
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              postId={post.id}
+              avatar={post.avator}
+              image={post.image}
+              text={post.text}
+              timestamp={post.timestamp}
+              username={post.username}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
